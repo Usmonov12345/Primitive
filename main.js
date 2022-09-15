8c8956ad7f126e0a057aea287a87f6df3516c887
Usmonov12345 <toxirusmonov04@gmail.com>
2022-09-15 15:01:07 +0500

    first commit

---
+++ main.js
@@ -0,0 +1,66 @@
+// //  "STRING"
+
+
+let name = "Toxir";
+
+console.log(name);
+console.log(typeof name);
+
+
+
+// // NUMBER
+
+
+let count = 20;
+let prise = 55.33;
+
+console.log(count);
+console.log(prise);
+
+console.log(typeof count);
+console.log(typeof prise);
+
+
+
+
+// // Boolean
+// // true, false
+
+let isStudent = false;
+let isMarried = true;
+
+console.log(isMarried);
+console.log(isStudent);
+
+console.log(typeof isMarried);
+console.log(typeof isStudent);
+
+
+
+
+
+
+// undefined
+// qiymati yoq narsalarga ishlatamiz
+
+let lastName;
+console.log(lastName);
+console
+
+
+
+// null 
+
+
+let isColor = null;
+console.log(isColor);
+console.log(typeof isColor);
+
+
+// BigInt
+//juda kotta sonlarga iwlatamiz
+
+let bigNumber = 255n;
+
+console.log(bigNumber);
+console.log(typeof bigNumber);
